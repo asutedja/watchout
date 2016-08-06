3ca3e57a6cf1d2765becbcb660f7dcef20a1f958
asutedja <asutedja@gmail.com>
2016-08-06 14:20:48 -0700

    Add Spongebob theme

--- client/watchout.js
+++ client/watchout.js
@@ -5,12 +5,13 @@ var enemyCount = 10;
 var collisions = 0;
 var score = 0;
 var highscore = 0;
+var cStatus;
 
 for (var i = 0; i < enemyCount; i++) {
   enemies.push([Math.random() * 720, Math.random() * 625]);
 }
 
-var svg = d3.select('.board')
+var svg = d3.selectAll('.board')
     .append('svg')
     .attr('height', 630)
     .attr('width', 720)
@@ -21,29 +22,42 @@ var drag = d3.drag()
   .on( 'drag', function(d, i) {
     d.x += d3.event.dx;
     d.y += d3.event.dy;
-    d3.select(this).attr('transform', function(d, i) {
-      return 'translate(' + [ d.x, d.y ] + ')';
+    d3.select(this)
+    // .attr('transform', function(d, i) {
+    //   return 'translate(' + [ d.x, d.y ] + ')';
+    // })
+    .attr('cx', function (d) { 
+      return d.x; 
+    })
+    .attr('cy', function (d) { 
+      return d.y; 
     });
   });
 
 
-  
+
+
+
+var updateData = function (data) {
 
 var player = svg.selectAll('.hero')
             .data(playerPos)
             .enter()
             .append('circle')
             .attr('class', 'hero')
-            .attr('transform', 'translate(' + playerPos[0].x + ',' + playerPos[0].y + ')')
+            //.attr('transform', 'translate(' + playerPos[0].x + ',' + playerPos[0].y + ')')
+            .attr('cx', function (d) { 
+              return d.x; 
+            })
+            .attr('cy', function (d) { 
+              return d.y; 
+            })
             .attr('r', 25)
-            .style('fill', 'blue')
+            .style('fill', 'url(#circles-2)')
             .style('z-index', -1 )
+            .style('position', 'absolute')
             .call(drag);
-
-
-var updateData = function (data) {
-
-
+            
   var enemy = svg.selectAll('.enemies')
     .data(data);
 
@@ -65,7 +79,7 @@ var updateData = function (data) {
     .attr('cy', function (d) { 
       return d[1]; 
     })
-    .style('fill', 'red');
+    .style('fill', 'url(#circles-1)');
 
   enemy.exit().remove();
 };
@@ -88,28 +102,47 @@ d3.interval(function() {
 var checkPos = function(data, hero) {
 
   for (var i = 0; i < data.length; i++) {
-    var distance = Math.sqrt((data[i][0] * data[i][0] - hero[0].x * hero[0].x) + ((data[i][1] * data[i][1] - hero[0].y * hero[0].y)));
-    if ( distance < 40) {
+    var distance = Math.sqrt(Math.pow(data[i][0] - hero[0].x, 2) + Math.pow(data[i][1] - hero[0].y, 2));
+    if ( distance < 40 && cStatus === false) {
       collisions++;
       score = 0;
+      cStatus = true;
     } 
   }
-
 };
 
-setInterval(function() {
+d3.interval(function() {
   checkPos(enemies, playerPos);
-}, 10);
+}, 100);
+// d3.timer(
+//   function() {
+
+//     for (var i = 0; i < enemies.length; i++) {
+//       var distance = Math.sqrt((enemies[i][0] * enemies[i][0] - playerPos[0].x * playerPos[0].x) + ((enemies[i][1] * enemies[i][1] - playerPos[0].y * playerPos[0].y)));
+//       if ( distance < 40 && cStatus === false) {
+//         collisions++;
+//         score = 0;
+//         cStatus = true;
+//       } 
+//     }
+//   }
+// , 10);
+
+d3.select('svg').html('<defs><pattern id="circles-1" patternUnits="objectBoundingBox" width="30" height="30"><image xlink:href="http://media2.giphy.com/media/SwWXo32l7mfaU/giphy.gif" x="0" y="0" width="30" height="30"></image></pattern><pattern id="circles-2" patternUnits="objectBoundingBox" width="50" height="50"><image xlink:href="http://myfirstchat.com/myfirstworld/aug13/3910.gif" x="0" y="0" width="50" height="50"></image></pattern></defs>').append();
+
 
 setInterval(function () {
   score = score + 10;
   if (score > highscore) {
     highscore = score;
   }
+  cStatus = false;
   d3.select('.current').text('Current score: ' + score);
   d3.select('.collisions').text('Collisions: ' + collisions);
   d3.select('.highscore').text('High score: ' + highscore);
 }, 1000);
+
+
 
 
 
